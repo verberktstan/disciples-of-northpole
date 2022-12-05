92b8d155fee25dcbdb9dfade1248ea6cc024fd5d
verberktstan <stan@brightin.nl>
2022-12-05 20:22:10 +0100

    Day one - refactor max-calories to sum multiple elves' calories

--- src/aoc22/day_one.clj
+++ src/aoc22/day_one.clj
@@ -3,17 +3,28 @@
    [clojure.edn :as edn]
    [clojure.java.io :as io]))
 
-(defn read-lines [filename]
+(defn- read-lines [filename]
   (with-open [reader (io/reader filename)]
     (doall (line-seq reader))))
 
-(defn max-calories [lines]
+(defn- sum-calories-per-elf [lines]
   (->> lines
        (map edn/read-string)
        (partition-by nil?)
-       (keep (partial reduce +))
-       (reduce max)))
+       (keep (partial reduce +))))
+
+(def read-and-sum (comp sum-calories-per-elf read-lines))
+
+(defn max-calories
+  ([calory-sums]
+   (reduce max calory-sums))
+  ([calory-sums n]
+   (->> calory-sums
+        (sort-by identity >)
+        (take n)
+        (reduce +))))
 
 (comment
-  (-> "resources/day-one-input.txt" read-lines max-calories)
+  (-> "resources/day-one-input.txt" read-and-sum max-calories)
+  (-> "resources/day-one-input.txt" read-and-sum (max-calories 3))
   )
