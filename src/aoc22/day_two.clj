5f740807abe7cb7dafaeb58d81c4abcd19b30f07
verberktstan <stan@brightin.nl>
2022-12-06 14:29:20 +0100

    Refactor: clean up and simplify day two, part one

--- src/aoc22/day_two.clj
+++ src/aoc22/day_two.clj
@@ -2,46 +2,32 @@
   (:require [aoc22.utils :refer [read-lines]]
             [clojure.string :as str]))
 
-(def parse-opponents-play {"A" :rock
-                           "B" :paper
-                           "C" :scissors})
+(def parse-play
+  {"A" :rock "B" :paper "C" :scissors "X" :rock "Y" :paper "Z" :scissors})
 
-(def parse-my-play {"X" :rock
-                    "Y" :paper
-                    "Z" :scissors})
-
-(def beats? {:rock     :scissors
-             :paper    :rock
-             :scissors :paper})
+(def beats? {:rock :scissors, :paper :rock, :scissors :paper})
 
 (def shape-score {:rock 1 :paper 2 :scissors 3})
 
 (defn result [{:keys [my-play opponents-play]}]
   (cond
-    (= my-play opponents-play)
-    :draw
+    (= my-play opponents-play)          :draw
+    (= (beats? my-play) opponents-play) :win
+    :else                               :loss))
 
-    (= (beats? my-play) opponents-play)
-    :win
-
-    :else
-    :loss))
-
-(def round-score {:loss 0
-                  :draw 3
-                  :win  6})
+(def round-score {:loss 0 :draw 3 :win 6})
 
 (defn parse-plays [s]
-  (let [[opponent me] (str/split s #" ")
-        my-play       (parse-my-play me)
-        op-play       (parse-opponents-play opponent)]
-    {:my-play        my-play
-     :opponents-play op-play}))
+  (let [[opponent me] (str/split s #" ")]
+    {:my-play        (parse-play me)
+     :opponents-play (parse-play opponent)}))
 
 (defn calculate-score [{:keys [my-play] :as plays}]
   (+ (shape-score my-play) (-> plays result round-score)))
 
 (comment
+  ;; Part one
+  ;; Return the sum of the scores for all the rounds
   (->> "resources/day-two-input.txt"
        read-lines
        (map parse-plays)
