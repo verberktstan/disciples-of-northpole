c828100e1eb8d61a0c55178c701cd0e5dc746602
verberktstan <stan@brightin.nl>
2022-12-06 14:46:31 +0100

    Day two - Determine play based on should-i win/draw/lose

--- src/aoc22/day_two.clj
+++ src/aoc22/day_two.clj
@@ -3,11 +3,22 @@
             [clojure.string :as str]))
 
 (def parse-play
-  {"A" :rock "B" :paper "C" :scissors "X" :rock "Y" :paper "Z" :scissors})
+  {"A" :rock "B" :paper "C" :scissors "X" :lose "Y" :draw "Z" :win})
 
 (def beats? {:rock :scissors, :paper :rock, :scissors :paper})
 
 (def shape-score {:rock 1 :paper 2 :scissors 3})
+
+(defn determine-play [i-should opponents-play]
+  (condp = i-should
+    :draw opponents-play
+    :win  (-> (filter (comp #{opponents-play} val) beats?)
+              first
+              key)
+    :lose (->> (remove (comp #{opponents-play} val) beats?)
+               (remove (comp #{opponents-play} key))
+               first
+               key)))
 
 (defn result [{:keys [my-play opponents-play]}]
   (cond
@@ -18,15 +29,17 @@
 (def round-score {:loss 0 :draw 3 :win 6})
 
 (defn parse-plays [s]
-  (let [[opponent me] (str/split s #" ")]
-    {:my-play        (parse-play me)
-     :opponents-play (parse-play opponent)}))
+  (let [[opponent me]  (str/split s #" ")
+        i-should       (parse-play me)
+        opponents-play (parse-play opponent)]
+    {:my-play        (determine-play i-should opponents-play)
+     :opponents-play opponents-play}))
 
 (defn calculate-score [{:keys [my-play] :as plays}]
   (+ (shape-score my-play) (-> plays result round-score)))
 
 (comment
-  ;; Part one
+  ;; Part two
   ;; Return the sum of the scores for all the rounds
   (->> "resources/day-two-input.txt"
        read-lines
