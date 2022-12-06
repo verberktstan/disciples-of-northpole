4c1c5998ea06cfa5e521c71433dd0e09dca4f037
verberktstan <stan@brightin.nl>
2022-12-06 20:39:52 +0100

    Day four - Count fully contained ranges

--- src/aoc22/day_four.clj
+++ src/aoc22/day_four.clj
@@ -1,26 +1,34 @@
 (ns aoc22.day-four
   (:require [aoc22.utils :as u]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.set :as set]))
+
+(defn fully-contained? [[r1 r2]]
+  (let [sets (map set [r1 r2])]
+    (or (apply set/subset? sets)
+        (apply set/superset? sets)
+        nil)))
 
 (defn range-inc-end [start end]
   (range start (inc end)))
 
 (defn ->range [s]
   (->> s
-       (re-find #"(\d)\-(\d)")
+       (re-find #"(\d+)\-(\d+)")
        rest
        (map edn/read-string)
        (apply range-inc-end)))
 
-(defn split-pair
-  [s]
+(defn split-pair [s]
   (->> s
-       (re-matches #"(\d\-\d),(\d\-\d)")
+       (re-matches #"(\d+\-\d+),(\d+\-\d+)")
        rest))
 
 (comment
  (->> "resources/day-four-input.txt"
       u/read-lines
       (map split-pair)
-      (map (partial mapv ->range)))
+      (map (partial mapv ->range))
+      (keep fully-contained?)
+      count)
   )
