b527dbdfa428ab4134a917b52b23d16f9c75b043
verberktstan <stan@brightin.nl>
2022-12-06 12:37:58 +0100

    Day two - Implement score-for-my-play and beats/i-won?

--- src/aoc22/day_two.clj
+++ src/aoc22/day_two.clj
@@ -2,20 +2,29 @@
   (:require [aoc22.utils :refer [read-lines]]
             [clojure.string :as str]))
 
-(def opponents-play {"A" :rock
-                     "B" :paper
-                     "C" :scissors})
+(def parse-opponents-play {"A" :rock
+                           "B" :paper
+                           "C" :scissors})
 
-(def my-play {"X" :rock
-              "Y" :paper
-              "Z" :scissors})
+(def parse-my-play {"X" :rock
+                    "Y" :paper
+                    "Z" :scissors})
+
+(def beats {:rock     :scissors
+            :paper    :rock
+            :scissors :paper})
+
+(def score-for-my-play {:rock 1 :paper 2 :scissors 3})
 
 (defn parse-plays [s]
-  (let [[opponent me] (str/split s #" ")]
-    {:my-play        (my-play me)
-     :opponents-play (opponents-play opponent)}))
+  (let [[opponent me] (str/split s #" ")
+        play          (parse-my-play me)
+        op-play       (parse-opponents-play opponent)]
+    {:my-play           play
+     :opponents-play    op-play
+     :score-for-my-play (score-for-my-play play)
+     :i-won?            (= (beats play) op-play)}))
 
 (comment
-  (read-lines "resources/day-two-input.txt")
   (map parse-plays (read-lines "resources/day-two-input.txt"))
   )
