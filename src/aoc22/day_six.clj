162cf35420363e7dbd234a77c7b0c2d7ea8bfc42
verberktstan <stan@brightin.nl>
2022-12-08 19:34:29 +0100

    Day six - part two, scan for message marker

--- src/aoc22/day_six.clj
+++ src/aoc22/day_six.clj
@@ -1,19 +1,18 @@
 (ns aoc22.day-six
   (:require [aoc22.utils :as u]))
 
-(def ^:private marker-length 4)
+(def ^:private packet 4) ; Length of start-of-packet marker
+(def ^:private message 14) ; Length of start-of-message marker
 
-(defn- scan-for-marker-packet [line]
-  (->> line (partition marker-length 1) (map (comp #{marker-length} count set))))
+(defn- scan-for-marker [n]
+  (fn [line] (->> line (partition n 1) (map (comp #{n} count set)))))
 
-(def ^:private lock-signal
-  (comp
-    (map (partial take-while nil?))
-    (map count)
-    (map (partial + marker-length))))
+(defn- lock-signal [n]
+  (comp (map (partial take-while nil?)) (map count) (map (partial + n))))
 
-(def -main
-  (u/wrap-main
-    {:part-one #(-> (comp (map scan-for-marker-packet) lock-signal)
-                    (u/read-lines "resources/day-six-input")
-                    first)}))
+(defn- scan [n]
+  #(-> (comp (map (scan-for-marker n)) (lock-signal n))
+       (u/read-lines "resources/day-six-input.txt")
+       first))
+
+(def -main (u/wrap-main {:part-one (scan packet) :part-two (scan message)}))
